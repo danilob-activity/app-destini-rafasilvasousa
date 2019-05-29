1fe90cac0fe0b5a85947e6bf6edeff5714a18b34
Rafael Sousa <rafaelsousa.ja.adv@gmail.com>
2019-05-29 11:55:30 -0300

    atividade 28/05/2019

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.londonappbrewery.destini.models.Answer;
+import com.londonappbrewery.destini.models.Story;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -15,8 +18,15 @@ public class MainActivity extends AppCompatActivity {
     // TODO: Declare as variaveis aqui:
 
     //indice corrente da historia
-    private Story mStorySelected;
+    //private Story mStorySelected;
+    Story T1 = new Story(R.string.T1_Story);
+    Story T2 = new Story(R.string.T2_Story);
+    Story T3 = new Story(R.string.T3_Story);
+    Story T4 = new Story(R.string.T4_End);
+    Story T5 = new Story(R.string.T5_End);
+    Story T6 = new Story(R.string.T6_End);
 
+    private Story mStorySelected = T1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +34,46 @@ public class MainActivity extends AppCompatActivity {
 
 
         //TODO: Faça o link do layout com a activity
+        mStoryTextView = findViewById(R.id.storyTextView);
+        mAnswerTop = findViewById(R.id.buttonTop);
+        mAnswerBottom = findViewById(R.id.buttonBottom);
 
         //TODO:faça o mapeamento da história
 
-        // TODO: Coloque o evento do click do botão, caso precise colocar a visibilidade no botão invisivel utilize a função
-        // do botão setVisibility(View.GONE):
 
+        T1.setAnswerTop(new Answer(R.string.T1_Ans1, T3));
+        T1.setAnswerBottom(new Answer(R.string.T1_Ans2, T2));
+
+        T2.setAnswerTop(new Answer(R.string.T2_Ans1, T3));
+        T2.setAnswerBottom(new Answer(R.string.T2_Ans2, T4));
+
+        T3.setAnswerTop(new Answer(R.string.T3_Ans1, T6));
+        T3.setAnswerBottom(new Answer(R.string.T3_Ans2, T5));
+
+        mStoryTextView.setText(mStorySelected.getStoryID());
+        mAnswerTop.setText(mStorySelected.getAnswerTop().getAnswerID());
+        mAnswerBottom.setText(mStorySelected.getAnswerBottom().getAnswerID());
+
+
+        // TODO: Coloque o evento do click do botão, caso precise colocar a visibilidade no botão invisivel utilize a função
+
+        mAnswerTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateStory(mStorySelected.getAnswerTop().getChildStory());
+            }
+        });
+
+        mAnswerBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateStory(mStorySelected.getAnswerBottom().getChildStory());
+            }
+        });
+
+    }
+    public void updateStory(Story newStory){
+        mStorySelected = newStory;
 
     }
 
